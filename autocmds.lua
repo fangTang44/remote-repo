cf933870cc0acf94d71965ea203789b9f9f0a03b
fangTang44 <151126302+fangTang44@users.noreply.github.com>
2024-04-07 03:01:21 +0800

    Add files via upload

---
+++ autocmds.lua
@@ -0,0 +1,53 @@
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+-- Add any additional autocmds here
+vim.cmd([[
+autocmd BufNewFile *.cpp,*.[ch],*.sh,*.java,*.go exec ":call SetTitle()"
+"""定义函数SetTitle，自动插入文件头
+func SetTitle()
+    "如果文件类型为.sh文件
+    if &filetype == 'sh'
+        call setline(1,"\#########################################################################")
+        call append(line("."),   "\# File Name:    ".expand("%"))
+        call append(line(".")+1, "\# Author:       fangTang")
+        call append(line(".")+2, "\# mail:         18691171371@168.com")
+        call append(line(".")+3, "\# Created Time: ".strftime("%c"))
+        call append(line(".")+4, "\#########################################################################")
+        call append(line(".")+5, "\#!/bin/bash")
+        call append(line(".")+6, "")
+    else
+        call setline(1, "/*************************************************************************")
+        call append(line("."),   "> File Name:     ".expand("%"))
+        call append(line(".")+1, "> Author:        fangTang")
+        call append(line(".")+2, "> mail:          18691171371@163.com")
+        call append(line(".")+3, "> Created Time:  ".strftime("%c"))
+        call append(line(".")+4, "> Description:   ")
+        call append(line(".")+5, "************************************************************************/")
+        call append(line(".")+6, "")
+    endif
+    "新建文件后，自动定位到文件末尾
+    autocmd BufNewFile * normal G
+endfunc
+]])
+
+vim.cmd([[
+autocmd BufNewFile *.h,*.hpp exec ":call AddHHeader()"
+func AddHHeader()
+    let macro = "_".toupper(substitute(expand("%"), "[/.]", "_", "g"))."_"
+    "normal o
+    call setline(9, "#ifndef ".macro)
+    call setline(10, "#define ".macro)
+    call setline(11, "")
+    call setline(12, "#endif  // ".macro)
+endfunc
+]])
+
+-- 保存指针位置
+vim.cmd([[
+  if has("autocmd")
+  autocmd BufReadPost *
+  \ if line("'\"") > 0 && line("'\"") <= line("$") |
+  \ exe "normal g'\"" |
+  \ endif
+  endif
+]])
